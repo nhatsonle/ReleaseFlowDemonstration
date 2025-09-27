4067c0a55a8acea7fcb0405855920a2e93f9af50
dmqn <daoquan2k6@gmail.com>
2025-09-27 22:28:51 +0700

    Add view tasks feature

--- TaskManager.java
+++ TaskManager.java
@@ -40,4 +40,16 @@ public class TaskManager {
         tasks.add(new Task(description));
         System.out.println("Task added.");
     }
+    
+    private void viewTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks available.");
+        } else {
+            System.out.println("\nTasks:");
+            for (int i = 0; i < tasks.size(); i++) {
+                Task task = tasks.get(i);
+                System.out.println((i + 1) + ". " + task.getDescription());
+            }
+        }
+    }
 }
