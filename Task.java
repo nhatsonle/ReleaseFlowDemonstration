2414b04ed31dcc42978e10ea046f9a9d467b4462
your name <buithach4626@gmail.com>
2025-09-27 22:19:57 +0700

    Add task priority topic

--- Task.java
+++ Task.java
@@ -1,8 +1,13 @@
 public class Task {
     private String description;
+    private int priority; // 1: High, 2: Medium, 3: Low
 
-    public Task(String description) {
-        this.description = description;
+    public Task(String description, int priority) {
+    this.description = description;
+    this.priority = priority;
+    }
+    public int getPriority() {
+    return priority;
     }
 
     public String getDescription() {
